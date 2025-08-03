539af9001e330bcba6ab5cb876969035cc9b7cdd
Anurag <anuragtiwari32003@gmail.com>
2025-08-03 23:09:30 +0530

    CORS Filter error handling7

--- src/main/java/com/ticketApplication/filters/CORSFilter.java
+++ src/main/java/com/ticketApplication/filters/CORSFilter.java
@@ -4,9 +4,9 @@
  */
 package com.ticketApplication.filters;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.logging.Logger;
 import org.springframework.stereotype.Component;
