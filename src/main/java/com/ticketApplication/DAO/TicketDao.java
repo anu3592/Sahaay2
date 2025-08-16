b90432fcb2fd011c4351323ea740c3e961fca71a
Anurag <anuragtiwari32003@gmail.com>
2025-08-16 19:00:42 +0530

    solving api problem37

--- src/main/java/com/ticketApplication/DAO/TicketDao.java
+++ src/main/java/com/ticketApplication/DAO/TicketDao.java
@@ -144,6 +144,7 @@ public class TicketDao {
         Session session = factory.getCurrentSession();
         //LocalDateTime twoDaysAgo = LocalDateTime.now(ZoneOffset.UTC).minusMinutes(7);//minusDays(2);
         OffsetDateTime twoDaysAgo = OffsetDateTime.now(ZoneOffset.UTC).minusMinutes(7);
+        System.out.println("OffsetDateTime value -> "+twoDaysAgo);
         Query query = session.createNativeQuery("Select id, name, title, phone, category, address, problem_desc, status, created_at, assigned_to, user_id, encode(image, 'base64') as image from tickets WHERE status = :status AND created_at < :twoDaysAgo", Ticket.class);
         // MySQL query -> Query q = session.createQuery("from Ticket where status = :status and TIMESTAMPDIFF(DAY, created_at, NOW()) > 2");
 //        Query q = session.createQuery(
