8a87b6313337e748b47e894c9514dc78cb65994b
Anurag <anuragtiwari32003@gmail.com>
2025-08-17 22:43:28 +0530

    solving api problem49

--- src/main/java/com/ticketApplication/DAO/TicketDao.java
+++ src/main/java/com/ticketApplication/DAO/TicketDao.java
@@ -163,8 +163,8 @@ public class TicketDao {
         //q.setParameter("status", "pending");
         query.setParameter("status", "Pending");
         //query.setParameter("twoDaysAgo", twoDaysAgo);
-        query.setParameter("twoDaysAgo", twoDaysAgo.toInstant().toString());
-        System.out.println("OffsetDateTime value -> " + Timestamp.from(twoDaysAgo.toInstant()));
+        query.setParameter("twoDaysAgo", twoDaysAgo);
+        System.out.println("OffsetDateTime value -> " + twoDaysAgo.toInstant().toString());
         List<Ticket> tickets = query.getResultList();
         System.out.println("Escalated ticket lists ->" + tickets);
         for (int i = 0; i < tickets.size(); i++) {
